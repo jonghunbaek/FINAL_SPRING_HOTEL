cd08ebf2a5cde51207e9bf822bb69aeb92503bbb
jiny <zumod3naranja@gmail.com>
2022-08-12 17:38:58 +0900

    room 파트 수정 - 컨트롤러, nav 경로 및 예약페이지 레이아웃

--- src/main/java/com/sh/controller/RoomController.java
+++ src/main/java/com/sh/controller/RoomController.java
@@ -2,7 +2,6 @@ package com.sh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,29 +10,37 @@ public class RoomController {
 
 //  객실 메인페이지 
 	@GetMapping(path="/roomHome")
-	public String main() {
-		return "/room/roomHome";
+	public String home() {
+		
+		return "room/roomHome";
 	}
 
 //	객실 상세페이지
 	@GetMapping(path="/roomDetail")
 	public String detail() {
-		return "/room/roomDetail";
+		
+		return "room/roomDetail";
 	}
 	
 //	예약
-	//1
+	//1 - 호텔, 날짜, 인원 선택
 	@GetMapping(path="/roomRev1")
 	public String roomRev1() {
 		//
-		return "/room/roomRev1";
+		return "room/roomRev1";
 	}
 	
-	//2
+	//2 - 옵션 선택 
 	@GetMapping(path="/roomRev2")
 	public String roomRev2() {
 		//
-		return "/room/roomRev2";
+		return "room/roomRev2";
+	}
+	//3 - 고객 정보 입력 
+	@GetMapping(path="/roomRev3")
+	public String roomRev3() {
+		//
+		return "room/roomRev3";
 	}
 	
 }
