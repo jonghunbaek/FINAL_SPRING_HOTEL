28b656497e5bca132b004e06de17461afee7ed90
cappuccinoob <luxon123@naver.com>
2022-08-18 23:32:40 +0900

    프로필 수정, 비밀번호 변경

--- src/main/java/com/sh/controller/UserController.java
+++ src/main/java/com/sh/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.sh.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sh.exception.ApplicationException;
 import com.sh.service.UserService;
 import com.sh.utils.SessionUtils;
 import com.sh.vo.Grade;
-import com.sh.vo.PointHistory;
 import com.sh.vo.User;
-import com.sh.vo.UserPoint;
 import com.sh.web.form.UserModifyForm;
 
 @Controller
@@ -80,7 +74,6 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "user/modify";
 	}
-	
 	// 내 정보 -> 프로필 수정 -> 프로필 수정 폼
 	@PostMapping("/modifyform")
 	public String passwordCheck(@RequestParam("password") String password, Model model) {
@@ -90,24 +83,22 @@ public class UserController {
 		
 		return "user/modifyform";
 	}
-	
-	// 이메일 중복체크
+	// 프로필 수정 폼 - 이메일 중복체크
 	@PostMapping(path="/emailCheck")
 	@ResponseBody
 	public int emailCheck(@RequestParam("email") String email) {
 		int check = userService.emailCheck(email);
 		return check;
 	}
-	
-	// 프로필 변경
+	// 프로필 수정 폼 - 프로필 변경
 	@PostMapping(path="/modifyProfile")
-	public String modifyProfile(@ModelAttribute("userModifyForm") UserModifyForm userModifyForm, Model model) {
-		userService.modifyUserInfo(userModifyForm);
+	public String modifyProfile(@RequestParam("email") String email, @RequestParam("tel") String tel) {
 		User loginUser = (User) SessionUtils.getAttribute("LOGIN_USER");
-		User user = userService.getUserDetail(loginUser.getId());
-		model.addAttribute("user", user);
-		
-		return "redirect:/user/modifyform";
+		loginUser.setEmail(email);
+		loginUser.setTel(tel);
+		userService.modifyUserInfo(loginUser);
+
+		return "redirect:/user/modify";
 	}
 	
 	// 내 정보 -> 비밀번호 변경
@@ -115,6 +106,23 @@ public class UserController {
 	public String changepw() {
 		
 		return "user/changePw";
+	}
+	// 비밀번호 변경 폼 - 기존 password 체크
+	@PostMapping(path="/passwordCheck")
+	@ResponseBody
+	public int passwordCheck(@RequestParam("password") String password) {
+		User loginUser = (User) SessionUtils.getAttribute("LOGIN_USER");
+		int check = userService.passwordCheck(loginUser.getId(), password);
+		return check;
+	}
+	// 비밀번호 변경 폼 - 비밀번호 변경
+	@PostMapping("/passwordChange")
+	public String passwordChange(@RequestParam("password2") String password) {
+		User loginUser = (User) SessionUtils.getAttribute("LOGIN_USER");
+		loginUser.setPassword(password);
+		userService.modifyUserInfo(loginUser);
+		
+		return "redirect:/user/changePw";
 	}
 	
 	// 내 정보 -> 문의 내역
--- src/main/java/com/sh/mapper/ProductMapper.java
+++ src/main/java/com/sh/mapper/ProductMapper.java
@@ -2,17 +2,18 @@ package com.sh.mapper;
 
 import java.util.List;
 
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.sh.vo.Product;
-import com.sh.vo.ProductCategory;
+//import com.sh.vo.ProductCategory;
 
 @Mapper
 public interface ProductMapper {
 	
 	Product getProductByNo(int no);
 	List<Product> getProductsByName(String name);
-	List<Product> getProductByShopCategoryNo(ProductCategory shopCategoryNo);
+	//List<Product> getProductByShopCategoryNo(ProductCategory shopCategoryNo);
 	List<Product> getProductByPrice(int price);
 	
 	void insertProduct(Product product);
